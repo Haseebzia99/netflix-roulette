cb8959beb50c6716f52634d5b860fab4d213f2cf
Haseeb Zia <haseeb_zia@epam.com>
2021-02-11 16:13:37 +0000

    search bar created and styling provided

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { AddMovie } from './components/AddMovie';
-import logo from './assets/images/netflix-logo.png'
+import logo from './assets/images/netflix-logo.png';
+import { SearchBanner } from './components/Banners/SearchBanner/index';
 
 function App() {
   return (
     <div className="App">
       <div className="netflix-header">
-        <img src={logo} className="netflix--logo"/>
+        <img src={logo} className="netflix--logo" alt="nt-log"/>
         <AddMovie />
+        <SearchBanner />
       </div>
     </div>
   );
---
+++ src/components/Search/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { Button } from "../../styles/buttonStyles";
+import { SearchContainer } from "./indexStyles";
+// import { fetchSearch } from "../../actions/moviesAction";
+// import { useDispatch } from "react-redux";
+
+export const Search = () => {
+//   const dispatch = useDispatch();
+  const [textInput, setTextInput] = useState("");
+  const inputHandler = (e) => {
+    setTextInput(e.target.value);
+  };
+
+//   const submitSearch = (e) => {
+//     e.preventDefault();
+//     dispatch(fetchSearch(textInput));
+//     setTextInput("");
+//   };
+
+  return (
+    <SearchContainer>
+      <input
+        value={textInput}
+        onChange={inputHandler}
+        placeholder="What do you want to watch?"
+        type="text"
+      />
+      <Button
+        // onClick={textInput.length ? submitSearch : null}
+        type="submit"
+        primary
+      >
+        SEARCH
+      </Button>
+    </SearchContainer>
+  );
+};
