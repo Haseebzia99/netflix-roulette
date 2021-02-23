d037a4232b746a354bdc8698b5eac410b526eab6
Haseeb Zia <haseeb_zia@epam.com>
2021-02-23 16:42:55 +0000

    formik populated with placeholders

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { SearchBanner } from "./components/Banners/SearchBanner/index";
 import "./App.css";
 
 function App() {
+
   return (
     <div className="App">
       <div className="netflix-header">
