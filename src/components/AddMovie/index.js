1408044e5f731ec8748094fe9e5b8e875e5d59be
Haseeb Zia <haseeb_zia@epam.com>
2021-02-26 14:25:14 +0000

    jsx refactored code

--- src/components/AddMovie/index.js
+++ src/components/AddMovie/index.js
@@ -49,65 +49,68 @@ export const AddMovie = () => {
     { key: "Family", value: "family" },
     { key: "Fantasy", value: "fantasy" },
   ];
+  
+  const renderFormik = (formik) => {
+    return (
+      <ModalContainer>
+        <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.TITLE]}</ModalTitle>
+        <ModalInput name="title" type="text" />
+        <ErrorText name="title" component="div" />
+
+        <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.RELEASE_DATE]}</ModalTitle>
+        <SelectorContainer>
+          <DateSelector
+            control="release_date"
+            name="date"
+            placeholderText={TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.SELECT_DATE]}
+          />
+          <IconBox size={20} />
+        </SelectorContainer>
+        <ErrorText name="date" component="div" />
+
+        <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.MOVIE_URL]}</ModalTitle>
+        <ModalInput name="poster_path" type="text" />
+        <ErrorText name="poster_path" component="div" />
+
+        <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.GENRE]}</ModalTitle>
+        <SelectorContainer>
+          <DropDown
+            control="select"
+            name="genres"
+            options={dropdownOptions}
+          />
+          <ArrowBox size={20} />
+        </SelectorContainer>
+        <ErrorText name="genre" component="div" />
+
+        <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.OVERVIEW]}</ModalTitle>
+        <ModalInput name="overview" type="text" />
+        <ErrorText name="overview" component="div" />
+
+        <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.RUNTIME]}</ModalTitle>
+        <ModalInput name="runtime" type="text" />
+        <ErrorText name="runtime" component="div" />
+
+        <ButtonContainer>
+          <ResetButton onClick={onReset}>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.RESET]}</ResetButton>
+          <SubmitButton type="submit" primary>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.SUBMIT]}</SubmitButton>
+        </ButtonContainer>
+
+      </ModalContainer>
+    );
+  };
 
   return (
     <AddMovieButtonContainer>
+
       <AddMovieButton red onClick={() => setIsOpen(true)}>
       {TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.ADD_MOVIE]}
       </AddMovieButton>
 
       <AddMovieModal isOpen={isOpen} onClose={() => setIsOpen(false)}>
       <Formik initialValues={initialValues} >
-      {(formik) => {
-        return (
-          <ModalContainer>
-            <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.TITLE]}</ModalTitle>
-            <ModalInput name="title" type="text" />
-            <ErrorText name="title" component="div" />
-
-            <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.RELEASE_DATE]}</ModalTitle>
-            <SelectorContainer>
-              <DateSelector
-                control="release_date"
-                name="date"
-                placeholderText={TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.SELECT_DATE]}
-              />
-              <IconBox size={20} />
-            </SelectorContainer>
-            <ErrorText name="date" component="div" />
-
-            <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.MOVIE_URL]}</ModalTitle>
-            <ModalInput name="poster_path" type="text" />
-            <ErrorText name="poster_path" component="div" />
-
-            <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.GENRE]}</ModalTitle>
-            <SelectorContainer>
-              <DropDown
-                control="select"
-                name="genres"
-                options={dropdownOptions}
-              />
-              <ArrowBox size={20} />
-            </SelectorContainer>
-            <ErrorText name="genre" component="div" />
-
-            <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.OVERVIEW]}</ModalTitle>
-            <ModalInput name="overview" type="text" />
-            <ErrorText name="overview" component="div" />
-
-            <ModalTitle>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.RUNTIME]}</ModalTitle>
-            <ModalInput name="runtime" type="text" />
-            <ErrorText name="runtime" component="div" />
-
-            <ButtonContainer>
-              <ResetButton onClick={onReset}>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.RESET]}</ResetButton>
-              <SubmitButton type="submit" primary>{TRANSLATIONS[I18N_LANGUAGES.EN][I18N_KEYS.SUBMIT]}</SubmitButton>
-            </ButtonContainer>
-
-          </ModalContainer>
-        );
-      }}
-    </Formik>
+        {renderFormik}
+      </Formik>
       </AddMovieModal>
 
     </AddMovieButtonContainer>
--- src/components/AddMovie/select.js
+++ src/components/AddMovie/select.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Field } from "formik";
 
+const renderOption = (option) => {
+  return (
+    <option key={option.value} value={option.value}>
+      {option.key}
+    </option>
+  );
+};
+
+const renderOptions = (options) => options.map(renderOption);
+
 function Select(props) {
   const { label, name, options, ...rest } = props;
   return (
     <div className="form-control">
       <label htmlFor={name}>{label}</label>
       <Field as="select" id={name} name={name} {...rest}>
-        {options.map((option) => {
-          return (
-            <option key={option.value} value={option.value}>
-              {option.key}
-            </option>
-          );
-        })}
+        {renderOptions(options)}
       </Field>
     </div>
   );
